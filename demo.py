3b7f5fcdd414d81a440169b385b6738b56810586
Miroslav Purkrabek <mira.purkrabek@gmail.com>
2022-11-21 16:18:12 +0100

    run demo also on the CMP Server

--- demo.py
+++ demo.py
@@ -10,9 +10,10 @@ my_mesh = Mesh(
     filename = os.path.join("demo_data", "smpl_uv.obj"),
 )
 my_mesh.set_vertex_colors("white")
+my_mesh.set_texture_image(os.path.join("demo_data", "smpl_uv.png"))
 
 # creates a grid of 2x2 mesh viewers
-mvs = MeshViewers()
+# mvs = MeshViewers()
 
 vis1, n_dot_cam1 = visibility_compute(
     v=my_mesh.v,
@@ -22,13 +23,18 @@ vis1, n_dot_cam1 = visibility_compute(
 
 # vis = my_mesh.vertex_visibility(camera=[0.0, 0.0, 0.0])
 vis1 = np.squeeze(vis1).astype(bool)
-print(my_mesh.v.shape)
+print(my_mesh.v.shape, my_mesh.v.dtype)
+print(my_mesh.f.shape, my_mesh.f.dtype)
+print(np.array([[0.0, 0.0, 0.0]]).shape)
+
 print(vis1.shape)
 # print(n_dot_cam.shape)
 
 my_mesh.set_vertex_colors("green", vertex_indices=vis1)
 my_mesh.set_vertex_colors("red", vertex_indices=~vis1)
 
+print(my_mesh.texture_image)
+
 # sets the first (top-left) mesh to my_mesh
-mvs[0][0].set_static_meshes([my_mesh])
+# mvs[0][0].set_static_meshes([my_mesh])
 
